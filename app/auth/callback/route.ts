80d99138ae12ffdc9833be7e1828deee5c2fb760
lanmower <almagestfraternite@gmail.com>
2024-09-23 15:15:28 +0200

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -30,7 +30,7 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
       return NextResponse.redirect(`${origin}${next}`)
-    }
+    } else NextResponse.json(error)
   }
 
   // return the user to an error page with instructions
